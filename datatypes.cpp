f322ef31e04ee33e5f7f608acc38c2c062891943
Niharika Pawar <119047940+niharikapawar03@users.noreply.github.com>
2025-02-28 20:27:22 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n=10;
+	cout<<n<<endl;
+	long x=10;
+	cout<<x<<endl;
+	long long y=1000;
+	cout<<y<<endl;
+	string s="niharika";
+	cout<<s<<endl;
+	float f=5.78;
+	cout<<f<<endl;
+	double d=5;
+	cout<<d<<endl;
+	string str;
+	getline(cin,str);
+	cout<<str;
+	
+	
+}
